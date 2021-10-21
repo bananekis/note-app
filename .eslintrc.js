4da2ec52ed1731c3f21c79e94ebbdc799427f70b
bananekis <lukes147@gmail.com>
2021-10-21 17:06:26 +0200

    prettier fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,6 @@ module.exports = {
   extends: [
     // 'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "plugin:react/recommended",
-    "prettier",
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     "plugin:react-hooks/recommended",
   ],
   parserOptions: {
